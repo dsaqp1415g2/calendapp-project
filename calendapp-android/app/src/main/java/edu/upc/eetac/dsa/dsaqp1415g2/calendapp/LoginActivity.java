c89d75a579d319d24826e6f53476e284c7fd1ec8
Jordi <Jordi@Jordi-PC>
2015-06-07 12:49:59 +0200

    Register Funcionando

--- calendapp-android/app/src/main/java/edu/upc/eetac/dsa/dsaqp1415g2/calendapp/LoginActivity.java
+++ calendapp-android/app/src/main/java/edu/upc/eetac/dsa/dsaqp1415g2/calendapp/LoginActivity.java
@@ -1,20 +1,31 @@
 package edu.upc.eetac.dsa.dsaqp1415g2.calendapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.upc.eetac.dsa.dsaqp1415g2.calendapp.api.AppException;
+import edu.upc.eetac.dsa.dsaqp1415g2.calendapp.api.CalendappAPI;
+import edu.upc.eetac.dsa.dsaqp1415g2.calendapp.api.User;
 
 /**
  * Created by Jordi on 29/05/2015.
  */
+
 public class LoginActivity extends Activity {
     private final static String TAG = LoginActivity.class.getName();
+
+User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,36 +49,85 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login_layout);
     }
 
-    public void signIn(View v) {
-        EditText etUsername = (EditText) findViewById(R.id.etUsername);
+    public void signIn(View v) throws AppException {
+
+
+        EditText etUsername = (EditText) findViewById(R.id.etUsername); // Obtener campos de texto de usuario y contraseña
         EditText etPassword = (EditText) findViewById(R.id.etPassword);
 
-        String username = etUsername.getText().toString();
-        String password = etPassword.getText().toString();
+        final String username = etUsername.getText().toString(); // Obtener usuario y contraseña
+        final String password = etPassword.getText().toString();
 
-        // Launch a background task to check if credentials are correct
-        // If correct, store username and password and start Beeter activity
-        // else, handle error
+        // Se debería acceder a la API y comprobar que las credenciales son correctas
 
-        // I'll suppose that u/p are correct:
-        SharedPreferences prefs = getSharedPreferences("calendapp-profile",
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.clear();
-        editor.putString("username", username);
-        editor.putString("password", password);
-        boolean done = editor.commit();
-        if (done)
-            Log.d(TAG, "preferences set");
-        else
-            Log.d(TAG, "preferences not set. THIS A SEVERE PROBLEM");
 
-        startCalendappActivity();
+        (new checkLoginTask()).execute(username, password);
 
-         }
-    private void startCalendappActivity() {
-        Intent intent = new Intent(this, CalendappMainActivity.class);
-        startActivity(intent);
-        finish();
+// I'll suppose that u/p are correct:
     }
+
+    private void startuTrollActivity() {
+        String urlUser = user.getLinks().get("self").getTarget();
+
+        Intent intent = new Intent(this, CalendappMainActivity.class);
+        intent.putExtra("url", urlUser);
+        startActivity(intent);
+        finish(); //Si no acabamos la actividad de Login, al darle al botón "back" en el móvil volvería a ella
+    }
+
+
+    private void evaluateLogin(Boolean loginOK) {
+        if (loginOK) {
+            EditText etUsername = (EditText) findViewById(R.id.etUsername);
+            EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+            final String username = etUsername.getText().toString();
+            final String password = etPassword.getText().toString();
+
+            SharedPreferences prefs = getSharedPreferences("Calendapp-profile",
+                    Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.clear();
+            editor.putString("username", username);
+            editor.putString("password", password);
+            boolean done = editor.commit();
+            if (done)
+                Log.d(TAG, "preferences set");
+            else
+                Log.d(TAG, "preferences not set. THIS A SEVERE PROBLEM");
+
+            startCalendappActivity();
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "El usuario o la contraseña son incorrectos";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+    }
+
+
+
+        private class checkLoginTask extends AsyncTask<String, Void, Boolean> {
+
+            private ProgressDialog pd;
+
+            @Override
+            protected Boolean doInBackground(String... params) {
+                Boolean correctLogin = false;
+                try {
+                    user = CalendappAPI.getInstance(LoginActivity.this)
+                            .checkLogin(params[0], params[1]);
+                    correctLogin = user.isLoginSuccessful();
+                } catch (AppException e) {
+                    e.printStackTrace();
+                }
+                return correctLogin;
+            }
+    }
+
+
 }
