4e9f75432d12d191b1b505504961e52058e21252
angel <angel@angel-TravelMate-B113>
2015-06-07 12:47:11 +0200

    InjectLinks de User/user collection

--- calendapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g2/calendapp/api/model/UserCollection.java
+++ calendapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g2/calendapp/api/model/UserCollection.java
@@ -3,7 +3,27 @@ package edu.upc.eetac.dsa.dsaqp1415g2.calendapp.api.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.core.Link;
+
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLink.Style;
+import org.glassfish.jersey.linking.InjectLinks;
+
+import edu.upc.eetac.dsa.dsaqp1415g2.calendapp.api.MediaType;
+import edu.upc.eetac.dsa.dsaqp1415g2.calendapp.api.UserResource;
+
 public class UserCollection {
+	@InjectLinks({
+		@InjectLink(resource = UserResource.class, style = Style.ABSOLUTE, rel = "create-user", title = "Create User", type = MediaType.CALENDAPP_API_USER, method = "createUser")
+	})
+	private List<Link> links;
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
 	private List<User> users;
 
 	public UserCollection() {
