eaf9459c21271ca1b4189c56e751bbfc68d450fb
Jordi <Jordi@Jordi-PC>
2015-06-10 23:37:19 +0200

    Crear eventos - android falta pulir

--- calendapp-android/app/src/main/java/edu/upc/eetac/dsa/dsaqp1415g2/calendapp/RegisterActivity.java
+++ calendapp-android/app/src/main/java/edu/upc/eetac/dsa/dsaqp1415g2/calendapp/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
